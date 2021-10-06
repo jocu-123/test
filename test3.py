ab82a0e8993c7a2468e42c9dab1e0f5d70acc6d0
jocu-123 <88067819+jocu-123@users.noreply.github.com>
2021-10-06 19:33:09 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,6 +1,6 @@
 #You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
 
-string="Hiall"
+string="Hi all"
 string1=string.upper()
 print(string1)
 string2=string.lower()
