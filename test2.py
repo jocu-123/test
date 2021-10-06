e8ade77f99b3dad2e2e4bab86f9b2f71d4916772
jocu-123 <88067819+jocu-123@users.noreply.github.com>
2021-10-06 19:31:30 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,3 +1,6 @@
+'''The user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. 
+String traversal will take place from left to right, not from right to left. String letters are case sensitive.'''
+
 def count_substring(string, sub_string):
     count=0
     for i in range(len(string)):
